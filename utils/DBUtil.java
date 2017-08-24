39431e3eb6bf4c7c24c7981ecbd3eb5250962590
lzhaoyang <575563079@qq.com>
2017-08-24 09:45:10 +0800

    增加了数据库连接工具类

--- utils/DBUtil.java
+++ utils/DBUtil.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
  * Created by Administrator on 2017/8/24.
  */
 public class DBUtil {
+    //静态变量
     static String ip = "127.0.0.1";
     static int port = 3306;
     static String database = "hutubill";
@@ -15,6 +16,7 @@ public class DBUtil {
     static String loginName = "root";
     static String password = "root";
 
+    //静态块，只加载一次
     static {
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -23,9 +25,11 @@ public class DBUtil {
         }
     }
 
+    //返回数据库连接对象
     public static Connection getConnection() throws SQLException{
         String url = String.format("jdbc:mysql://%s:%d/%s?characterEncoding=%s",ip,
                                         port,database,encoding,loginName,password);
         return DriverManager.getConnection(url);
     }
+
 }
